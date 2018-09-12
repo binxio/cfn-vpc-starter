fa35fab8f62d379a60a91a5944454139bbae85f2
Martijn van Dongen <martijnvandongen@binx.io>
2018-09-12 07:33:21 +0200

    add layered 2az

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 jinja2 vpc.yml.j2 data/public-1az.yml --format=yaml > templates/public-1az.yml
 jinja2 vpc.yml.j2 data/public-3az.yml --format=yaml > templates/public-3az.yml
 jinja2 vpc.yml.j2 data/layered-1az.yml --format=yaml > templates/layered-1az.yml
+jinja2 vpc.yml.j2 data/layered-2az.yml --format=yaml > templates/layered-2az.yml
 jinja2 vpc.yml.j2 data/layered-3az.yml --format=yaml > templates/layered-3az.yml
 jinja2 vpc.yml.j2 data/layered-5az.yml --format=yaml > templates/layered-5az.yml
 jinja2 vpc.yml.j2 data/large.yml --format=yaml > templates/large.yml
